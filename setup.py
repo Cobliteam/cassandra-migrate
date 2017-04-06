dee06335b568cb2f32eddcb8b348614d6f133feb
Daniel Miranda <danielkza2@gmail.com>
2017-04-06 17:41:43 -0300

    Update packaging

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
-VERSION = '0.1'
+VERSION = '0.1.1'
 
 setup(name='cassandra-migrate',
       packages=['cassandra_migrate'],
       version=VERSION,
       description='Simple Cassandra database migration program',
+      long_description=open('README.rst').read(),
       url='https://github.com/Cobliteam/cassandra-migrate',
       download_url='https://github.com/Cobliteam/cassandra-migrate/archive/{}.tar.gz'.format(VERSION),
       author='Daniel Miranda',
