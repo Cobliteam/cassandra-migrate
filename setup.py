0cb221947fb8a8fb6fe7ac77e3030875683c8493
Ivan Stoiev <ivan.stoiev@cobli.co>
2021-09-29 05:38:27 -0300

    Release 0.3.5

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-VERSION = '0.3.4'
+VERSION = '0.3.5'
 
 install_requires = [
     "arrow==0.17.0",
     "cassandra-driver==3.24.0",
-    "cassandra-migrate==0.3.4",
+    "cassandra-migrate==0.3.5",
     "click==7.1.2",
     "future==0.18.2",
     "geomet==0.2.1.post1",
