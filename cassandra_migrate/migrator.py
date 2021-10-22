5042daf72771013676de546662b9aba00da8170d
Artemiy Firsov <a.firsov@velvetech.ru>
2021-10-22 18:13:39 +0700

    remove timeouts

--- cassandra_migrate/migrator.py
+++ cassandra_migrate/migrator.py
@@ -141,9 +141,6 @@ class Migrator(object):
             contact_points=hosts,
             port=port,
             auth_provider=auth_provider,
-            max_schema_agreement_wait=300,
-            control_connection_timeout=10,
-            connect_timeout=30,
             ssl_options=ssl_options,
             **config.cluster_connection_properties)
 
